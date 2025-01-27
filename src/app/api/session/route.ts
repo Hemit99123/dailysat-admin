6d24d1f8951f8106fd32bc5f13bf19abe5f6d046
Hemit Patel <hemitvpatel@gmail.com>
2025-01-27 14:52:28 -0500

    added some comments

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -3,12 +3,17 @@ import jwt from "jsonwebtoken";
 
 export const POST = async (request: Request) => {
     const body = await request.json(); // Await the request body
+
+    // Get the jwt token
     const token = body.token;
     let message;
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET || "default");
 
+        // ts guard + checking if token is valid
+        // if not then user is not authenticated for a session and thus setSession() shall not run
+        
         if (typeof decoded === "object" && "email" in decoded) {
             const result = await setSession(decoded.email as string);
             message = result ? "Session added" : "Session was NOT added";
