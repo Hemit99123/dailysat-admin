6294ebe6cb5e0a44e07389734d249fbe4c12face
Hemit Patel <hemitvpatel@gmail.com>
2025-01-27 15:05:15 -0500

    added some comments to help devs

--- src/lib/middleware/checkSession.ts
+++ src/lib/middleware/checkSession.ts
@@ -10,7 +10,7 @@ const handleCheckSession = async (request: NextRequest) => {
 
         // different logic for login because we are redirecting to logout NOT login page
         // this is why we check for if current page is /login first then other logic 
-        
+
         if (request.nextUrl.pathname === '/login') {
             return isSessionValid ? redirectTo(request, '/logout') : null;
         }
@@ -24,7 +24,7 @@ const handleCheckSession = async (request: NextRequest) => {
         }
     }
 
-  
+    // if session IS valid, we return nada 
     return null;
 };
 
