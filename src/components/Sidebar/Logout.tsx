f880d1bb80f02c623599322bd6a9babb661fd2dd
Hemit Patel <hemitvpatel@gmail.com>
2025-01-28 14:21:58 -0500

    redirect to logout if asked

--- src/components/Sidebar/Logout.tsx
+++ src/components/Sidebar/Logout.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { redirect } from "next/navigation";
 
 const Logout = () => {
+
+  const handleGoToLogout = () => {
+    redirect("/logout")
+  }
   return (
     <div className="flex sticky top-[calc(100vh_-_48px_-_16px)] flex-col h-12 border-t px-2 border-stone-300 justify-end text-xs">
       <div className="flex items-center justify-between">
@@ -8,7 +13,7 @@ const Logout = () => {
           <p className="font-bold">Finished for the day?</p>
         </div>
 
-        <button className="px-2 py-1.5 font-medium bg-stone-200 hover:bg-stone-300 transition-colors rounded">
+        <button className="px-2 py-1.5 font-medium bg-stone-200 hover:bg-stone-300 transition-colors rounded" onClick={handleGoToLogout}>
           Logout
         </button>
       </div>
