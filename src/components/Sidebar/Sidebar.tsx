3a4a2e282a0f88f9bed565f707a351eed049cd1b
Hemit Patel <hemitvpatel@gmail.com>
2025-01-28 12:55:01 -0500

    removed search

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Search from "./Search";
 import RouteSelect from "./RouteSelect";
 import ContactUs from "./ContactUs";
 import DailySATHeader from "./DailySATHeader";
@@ -9,7 +8,6 @@ const Sidebar = () => {
     <div>
       <div className="overflow-y-scroll sticky top-4 h-[calc(100vh-32px-48px)]">
         <DailySATHeader />
-        <Search />
         <RouteSelect />
       </div>
 
