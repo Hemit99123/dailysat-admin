40c87d9d5245217db7485f2fe993cccd39b03e28
Hemit Patel <hemitvpatel@gmail.com>
2025-01-28 12:28:33 -0500

    added boilerplate admin panel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Dashboard } from "@/components/Dashboard/Dashboard";
+import { Sidebar } from "@/components/Sidebar/Sidebar";
+
 export default function Home() {
   return (
-    <div className="">
-      <h1>Admin Panel</h1>
-    </div>
+    <main className="grid gap-4 p-4 grid-cols-[220px,_1fr]">
+      <Sidebar />
+      <Dashboard />
+    </main>
   );
 }
