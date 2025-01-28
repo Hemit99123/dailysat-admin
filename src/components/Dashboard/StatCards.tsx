5f3c419ad70164727af98fd5d85208cd66452ea2
Hemit Patel <hemitvpatel@gmail.com>
2025-01-28 13:09:47 -0500

    added proper stats

--- src/components/Dashboard/StatCards.tsx
+++ src/components/Dashboard/StatCards.tsx
@@ -1,29 +1,40 @@
-import React from "react";
-import { FiTrendingDown, FiTrendingUp } from "react-icons/fi";
+"use client"
+
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const StatCards = () => {
+  const [mathQuestionNumber, setMathQuestionNumber] = useState(0)
+  const [readingWritingQuestionNumber, setReadingWritingQuestionNumber] = useState(0)
+  const [userCount, setUserCount] = useState(0)
+
+  useEffect(() => {
+    const getNumberQuestions = async () => {
+      const response = await axios.get("/api/stats")
+  
+      setMathQuestionNumber(response.data.mathCount)
+      setReadingWritingQuestionNumber(response.data.readingWritingCount)
+      setUserCount(response.data.userCount)
+    }
+
+    getNumberQuestions()
+  }, [])
+
+
+
   return (
     <>
       <Card
-        title="Gross Revenue"
-        value="$120,054.24"
-        pillText="2.75%"
-        trend="up"
-        period="From Jan 1st - Jul 31st"
+        title="Math Questions"
+        value={mathQuestionNumber.toString()}
       />
       <Card
-        title="Avg Order"
-        value="$27.97"
-        pillText="1.01%"
-        trend="down"
-        period="From Jan 1st - Jul 31st"
+        title="Reading/Writing Questions"
+        value={readingWritingQuestionNumber.toString()}
       />
       <Card
-        title="Trailing Year"
-        value="$278,054.24"
-        pillText="60.75%"
-        trend="up"
-        period="Previous 365 days"
+        title="Users"
+        value={userCount.toString()}
       />
     </>
   );
@@ -32,36 +43,18 @@ const StatCards = () => {
 const Card = ({
   title,
   value,
-  pillText,
-  trend,
-  period,
 }: {
   title: string;
   value: string;
-  pillText: string;
-  trend: "up" | "down";
-  period: string;
 }) => {
   return (
     <div className="col-span-4 p-4 rounded border border-stone-300">
-      <div className="flex mb-8 items-start justify-between">
+      <div className="flex items-start justify-between">
         <div>
           <h3 className="text-stone-500 mb-2 text-sm">{title}</h3>
           <p className="text-3xl font-semibold">{value}</p>
         </div>
-
-        <span
-          className={`text-xs flex items-center gap-1 font-medium px-2 py-1 rounded ${
-            trend === "up"
-              ? "bg-green-100 text-green-700"
-              : "bg-red-100 text-red-700"
-          }`}
-        >
-          {trend === "up" ? <FiTrendingUp /> : <FiTrendingDown />} {pillText}
-        </span>
       </div>
-
-      <p className="text-xs text-stone-500">{period}</p>
     </div>
   );
 };
