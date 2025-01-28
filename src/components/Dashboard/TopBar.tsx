92f5a9ba1532460b4133ff68e464cb8d44e3beb3
Hemit Patel <hemitvpatel@gmail.com>
2025-01-28 12:42:08 -0500

    updated topbar to be dynamic with information

--- src/components/Dashboard/TopBar.tsx
+++ src/components/Dashboard/TopBar.tsx
@@ -1,21 +1,39 @@
-import React from "react";
-import { FiCalendar } from "react-icons/fi";
+"use client"
+
+import { getSessionInfo } from "@/lib/auth/employeeSession";
+import React, { useEffect, useState } from "react";
+import { formatDate } from "@/lib/date/format";
+import { handleGetGreeting } from "@/lib/date/greeting";
 
 export const TopBar = () => {
+
+  /* eslint-disable  @typescript-eslint/no-explicit-any */
+  const [session, setSession] = useState<any>()
+
+  const date = new Date()
+  const formattedDate = formatDate(date)
+  const greeting = handleGetGreeting()
+
+  useEffect(() => {
+    const handleGetSessionInfo = async () => {
+      const session = await getSessionInfo()
+      setSession(session)
+    }
+
+    handleGetSessionInfo();
+  }, [])
+
+
+
   return (
     <div className="border-b px-4 mb-4 mt-2 pb-4 border-stone-200">
       <div className="flex items-center justify-between p-0.5">
         <div>
-          <span className="text-sm font-bold block">🚀 Good morning, Tom!</span>
+          <span className="text-xl font-bold block">🚀 {greeting}, {session?.email}!</span>
           <span className="text-xs block text-stone-500">
-            Tuesday, Aug 8th 2023
+            {formattedDate}
           </span>
         </div>
-
-        <button className="flex text-sm items-center gap-2 bg-stone-100 transition-colors hover:bg-violet-100 hover:text-violet-700 px-3 py-1.5 rounded">
-          <FiCalendar />
-          <span>Prev 6 Months</span>
-        </button>
       </div>
     </div>
   );
