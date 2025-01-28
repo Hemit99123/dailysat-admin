40fb0ce43c9e9abfae238ef00f40eb178472a60d
Hemit Patel <hemitvpatel@gmail.com>
2025-01-28 13:46:53 -0500

    added different page views

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
+"use client"
+
 import { Dashboard } from "@/components/Dashboard/Dashboard";
+import Item from "@/components/Item/Item";
+import Questions from "@/components/Question/Questions";
 import Sidebar from "@/components/Sidebar/Sidebar";
+import { usePageStore } from "@/store/pageStore";
 
 export default function Home() {
+  const page = usePageStore((state) => state.page); // Retrieve the current page from Zustand store
+
   return (
     <main className="grid gap-4 p-4 grid-cols-[220px,_1fr]">
       <Sidebar />
-      <Dashboard />
+
+      {page === "dashboard" ? <Dashboard /> : page === "questions" ? <Questions /> : page === "item" ? <Item /> : null}
+
     </main>
   );
 }
